39267917dceaf805bc69ca3ff09dfa4b81ab8c88
pc <pc@DESKTOP-T3L3RE2>
2023-10-27 10:13:51 +0700

    khoa

---
+++ BT_QuanLySinhVien/khoa.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace BT_QuanLySinhVien
+{
+    partial class khoa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            dataGridView1 = new DataGridView();
+            cl_MaKhoa = new DataGridViewTextBoxColumn();
+            cl_TenKhoa = new DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            label1.Location = new Point(302, 11);
+            label1.Name = "label1";
+            label1.Size = new Size(197, 30);
+            label1.TabIndex = 0;
+            label1.Text = "DANH MỤC KHOA";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(249, 77);
+            label2.Name = "label2";
+            label2.Size = new Size(68, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Mã Khoa";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(249, 152);
+            label3.Name = "label3";
+            label3.Size = new Size(70, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Tên Khoa";
+            // 
+            // textBox1
+            // 
+            textBox1.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox1.Location = new Point(374, 70);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(177, 27);
+            textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            textBox2.Font = new Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point);
+            textBox2.Location = new Point(374, 145);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(177, 27);
+            textBox2.TabIndex = 4;
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { cl_MaKhoa, cl_TenKhoa });
+            dataGridView1.Location = new Point(178, 204);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.RowTemplate.Height = 25;
+            dataGridView1.Size = new Size(444, 246);
+            dataGridView1.TabIndex = 5;
+            // 
+            // cl_MaKhoa
+            // 
+            cl_MaKhoa.HeaderText = "MaKhoa";
+            cl_MaKhoa.Name = "cl_MaKhoa";
+            cl_MaKhoa.Width = 200;
+            // 
+            // cl_TenKhoa
+            // 
+            cl_TenKhoa.HeaderText = "TenKhoa";
+            cl_TenKhoa.Name = "cl_TenKhoa";
+            cl_TenKhoa.Width = 200;
+            // 
+            // khoa
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(dataGridView1);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "khoa";
+            Text = "khoa";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn cl_MaKhoa;
+        private DataGridViewTextBoxColumn cl_TenKhoa;
+    }
+}
